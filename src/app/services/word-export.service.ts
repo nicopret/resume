1c1734b9b12908e5af6b2bb0176c46e0ff79645a
ct300599 <NicoPretorius@capitecbank.co.za>
2019-07-13 19:31:40 +0200

    Enable template download

--- src/app/services/word-export.service.ts
+++ src/app/services/word-export.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class WordExportService {
 
-    createDoc(data: any) {
+    download = new Subject();
+
+    constructor(private http: HttpClient) {}
+
+    async createDoc(data: any) {
         console.log(data);
+        const fileName = `Resume for ${data.profile.fullName}${data.filter.length ? ' - ' + data.filter + ' experience' : ''}.doc`;
+        let template = await this.getFile();
+
+        this.download.next({ content: template, fileName });
+    }
+
+    async getFile() {
+        return new Promise((resolve) => {
+            this.http.get('/assets/template.docx', { responseType: 'arraybuffer'}).subscribe((res) => resolve(res));
+        });
     }
 }

bb91d3c1d49948f1b4afef18b576ab47bc6b8c35
ct300599 <NicoPretorius@capitecbank.co.za>
2019-07-17 15:12:36 +0200

    Added modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
 import { WorkComponent } from './pages/workPage/work.component';
 import { ProfileComponent } from './pages/profilePage/profile.component';
 import { StatsComponent } from './pages/statsPage/stats.component';
 import { EducationComponent } from './pages/educationPage/education.component';
 import { SkillsComponent } from './pages/skillsPage/skills.component';
+import { ModalComponent } from './dashboard/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     EducationComponent,
+    ModalComponent,
     ProfileComponent,
     SkillsComponent,
     StatsComponent,
@@ -24,6 +27,7 @@ import { SkillsComponent } from './pages/skillsPage/skills.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule
   ],

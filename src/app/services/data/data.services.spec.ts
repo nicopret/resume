c6c969ff93bda58a24967f574fa57085b653f161
ct300599 <NicoPretorius@capitecbank.co.za>
2019-07-18 14:37:53 +0200

    Enable data load in service

--- src/app/services/data/data.services.spec.ts
+++ src/app/services/data/data.services.spec.ts
@@ -1,21 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
 import { DataService } from './data.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('Data service', () => {
 
+    const mockResume = {
+        profile: {}
+    };
+
+    let httpTestingController: HttpTestingController;
     let service: DataService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            providers: [
-                DataService
-            ]
+            imports: [ HttpClientTestingModule ],
+            providers: [ DataService ]
         }).compileComponents();
+
+        httpTestingController = TestBed.get(HttpTestingController);
+        service = TestBed.get(DataService);
     }));
 
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
     it('should create the service', () => {
-        service = TestBed.get(DataService);
         expect(service).toBeTruthy();
     });
 
+    it('should load the data file', () => {
+
+        service.init().subscribe((result) => {
+
+            expect(result['profile']).toBeTruthy();
+
+        });
+
+        const req = httpTestingController.expectOne('/assets/resume.json');
+
+        req.flush(mockResume);
+    });
 });

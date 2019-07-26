5f4d80fad46e2190c5af64cf2b5c6d79c198dbba
Nico Pretorius <nico.pretorius@pbtgroup.co.za>
2019-07-26 20:24:44 +0200

    Fixed testing errors

--- src/app/pages/skillsPage/skills.component.spec.ts
+++ src/app/pages/skillsPage/skills.component.spec.ts
@@ -1,17 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SkillsComponent } from './skills.component';
+import { DataService } from 'src/app/services/data/data.service';
+import { ApiService } from 'src/app/services/api/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ArrayUtilService } from 'src/app/services/util/arrayUtil.service';
 
 describe('Skills Component', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
-                RouterTestingModule
+                RouterTestingModule, HttpClientModule
             ],
             declarations: [
                 SkillsComponent
             ],
+            providers: [ ApiService, ArrayUtilService, DataService ]
         }).compileComponents();
     }));
 
